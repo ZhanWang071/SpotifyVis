6bee2f2ccb8ab90ca674c1d12b4f7159099939e3
Zhan Wang <wz986970406@gmail.com>
2022-12-13 12:54:46 +0800

    initialize framework

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,29 +3,43 @@
     <!-- <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/> -->
     <el-container style="height: 100%">
-      <el-header style="height: 8%"><h1>Spotify Visualization</h1></el-header>
-      <el-container style="height: 90%">
-        <el-aside width="30%" style="height: 100%">
-          <div style="height: calc(50% - 16px); padding: 6px">
-            <graph-component />
-          </div>
-          <div style="height: calc(50% - 16px); padding: 6px">
-            <map-component />
-          </div>
-        </el-aside>
-        <el-main
-          style="
-            padding-left: 6px;
-            padding-right: 8px;
-            padding-top: 6px;
-            padding-bottom: 14px;
-          "
-        >
+      <el-header style="height: 70px"
+        ><span class="title"
+          >Spotify - Visualization of Genre Trends</span
+        ></el-header
+      >
+      <el-container>
+        <el-main style="padding-left: 40px; padding-top: 6px; height: 954px">
           <div style="height: 100%">
             <spiral-component />
           </div>
         </el-main>
+        <el-aside width="26%" style="height: 100%">
+          <div style="height: 460px; padding-top: 6px; padding-right: 40px">
+            <graph-component />
+          </div>
+          <div style="height: 460px; padding-top: 8px; padding-right: 40px">
+            <map-component />
+          </div>
+        </el-aside>
       </el-container>
+      <el-footer height="50px">
+        <img
+          style="position: absolute; left: 27px; top: 1038px"
+          alt="Vue logo"
+          src="../assets/Spotify_logo.png"
+        />
+        <span class="dataSource"
+          >Credit to:
+          https://www.kaggle.com/datasets/jfreyberg/spotify-chart-data</span
+        >
+        <img
+          style="position: absolute; right: 40px; top: 1025px"
+          alt="Vue logo"
+          src="../assets/thumbnail_Outlook-bapckxvn.png"
+        />
+        <span class="footer">Spotify - Visualization of Genre Trends</span>
+      </el-footer>
     </el-container>
   </div>
 </template>
@@ -49,13 +63,69 @@ export default {
 <style scoped>
 .el-header {
   position: relative;
-  background-color: #2c2f3b;
+  /* background-color: #2c2f3b; */
   color: #fff;
   justify-content: center;
   display: flex;
   align-items: center;
 }
 .home {
-  height: 100%;
+  height: 1080px;
+  background-color: #171717;
+}
+.title {
+  position: absolute;
+  /* width: 608px; */
+  height: 47px;
+  /* left: 602px; */
+  top: 17px;
+
+  font-family: "DM Sans";
+  font-style: normal;
+  font-weight: 700;
+  font-size: 30px;
+  line-height: 47px;
+  /* identical to box height */
+
+  letter-spacing: -0.05em;
+
+  color: #ffffff;
+}
+.dataSource {
+  position: absolute;
+  /* width: 443px; */
+  height: 15px;
+  left: 150px;
+  top: 1046px;
+
+  font-family: "JejuGothic";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 12px;
+  line-height: 13px;
+  display: flex;
+  align-items: center;
+  text-align: center;
+
+  color: rgba(255, 255, 255, 0.3);
+}
+.footer {
+  position: absolute;
+  /* width: 239px; */
+  height: 15px;
+  left: 1476px;
+  top: 1046px;
+
+  font-family: "JejuGothic";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 12px;
+  line-height: 13px;
+  display: flex;
+  align-items: center;
+  text-align: center;
+  letter-spacing: -0.05em;
+
+  color: rgba(255, 255, 255, 0.3);
 }
 </style>
